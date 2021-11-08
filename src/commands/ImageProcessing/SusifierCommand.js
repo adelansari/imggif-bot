eff7c94b92f1943ff51d6d21a4e615002beed4d6
Adel Ansari <adelansari@github.com>
2021-11-08 23:57:33 +0200

    update

--- src/commands/ImageProcessing/SusifierCommand.js
+++ src/commands/ImageProcessing/SusifierCommand.js
@@ -1,15 +1,21 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
+// const { Client, Message, MessageAttachment} = require("discord.js")
 
 module.exports = class SusifierCommand extends BaseCommand {
   constructor() {
     super('susifier', 'ImageProcessing', []);
   }
 
-  run(client, message, args) {
+  async run (client, message, args) {
     message.channel.send('susifier command works');
 
-    twerk_frame_count = 6  // 0.png to 5.png
-    
+    twerk_frame_count = 6;  // 0.png to 5.png
+
+    const messageAttachment = message.attachments.size > 0 ? message.attachments.array()[0].url : null;
+    // Exit if there is no message attachment present in the message
+    if (!messageAttachment) return;
+
+
     // Loading frames:
 
 
