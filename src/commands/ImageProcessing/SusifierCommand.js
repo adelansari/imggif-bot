49b39e37a6c55b92565d8d9f02d02c2e8a1e8c0a
Adel Ansari <adelansari@github.com>
2021-11-17 01:57:26 +0200

    new changes

--- src/commands/ImageProcessing/SusifierCommand.js
+++ src/commands/ImageProcessing/SusifierCommand.js
@@ -3,6 +3,7 @@ const { Client, Message, MessageAttachment } = require("discord.js");
 const Canvas = require("canvas");
 const path = require("path");
 const fs = require("fs");
+const sizeOf = require('image-size')
 
 module.exports = class SusifierCommand extends BaseCommand {
   constructor() {
@@ -17,51 +18,78 @@ module.exports = class SusifierCommand extends BaseCommand {
 
     const attachedImage = message.attachments.first().url; // saving the image sent by the user
 
+    const sentImgWidth = attachedImage.width;
+    console.log(sentImgWidth)
+    const sentImgHeight = attachedImage.height;
+    console.log(sentImgHeight)
+
     // message.channel.send(attachedImage);
 
     // Creating a canvas of 64x64 from the attached image
+    // const canvas = Canvas.createCanvas(128, 128);
     const canvas = Canvas.createCanvas(128, 128);
     const ctx = canvas.getContext("2d");
     const background = await Canvas.loadImage(attachedImage);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height); // pixel data
+    const sample_size = 64;
 
-    const attachment = new MessageAttachment(
+    const rescaledImage = new MessageAttachment(
       canvas.toBuffer(),
       "sussified.png"
     );
-    message.channel.send({ files: [attachment] });
+    await message.channel.send("Rescaled Image:");
+    await message.channel.send({ files: [rescaledImage] });
 
     // Getting the color value per pixel
-    const data = ctx.getImageData(X, Y, 1, 1).data;
+    const xCoord = 0;
+    const yCoord = 0;
+    const data = ctx.getImageData(xCoord, yCoord, 1, 1).data;
+
+    // Pixelating the image:
+
+  
+
+
 
     // invert colors
     for (var i = 0; i < imgData.data.length; i += 4) {
-      imgData.data[i] = 255 - imgData.data[i];  // RED
-      imgData.data[i + 1] = 255 - imgData.data[i + 1];  // GREEN
-      imgData.data[i + 2] = 255 - imgData.data[i + 2];  // BLUE
-      imgData.data[i + 3] = 255;  // ALPHA
+      imgData.data[i] = 255 - imgData.data[i]; // RED
+      imgData.data[i + 1] = 255 - imgData.data[i + 1]; // GREEN
+      imgData.data[i + 2] = 255 - imgData.data[i + 2]; // BLUE
+      imgData.data[i + 3] = 255; // ALPHA
     }
 
     ctx.putImageData(imgData, 0, 0);
 
+    const invertedImage = new MessageAttachment(
+      canvas.toBuffer(),
+      "inverted.png"
+    );
+    await message.channel.send("Inverted Image:");
+    await message.channel.send({ files: [invertedImage] });
+
     // 128 is the height of the rescaled image:
     // formula to find the color value of a given pixel in the array: x+y*width*4
-    const imageDetailedData = []
+    const imageDetailedData = [];
     for (let y = 0; y < canvas.height; y++) {
       for (let x = 0; x < canvas.width; x++) {
-        const index = ( x + y * width ) * 4;
+        const index = (x + y * canvas.width) * 4;
       }
     }
 
+    const twerk_frame_count = 6; // 0.png to 5.png
+    const twerk_file_path = "/twerk_imgs/";
+
+    // gif output:
+    const output_width = 20;
+    const output_width_px = 20
+    const output_height = output_width * (input_height / input_width) * (twerk_width / twerk_height);
     
 
 
-    message.channel.send({ files: [attachment] });
 
 
-    const twerk_frame_count = 6; // 0.png to 5.png
-    const twerk_file_path = "/twerk_imgs/";
     // const imageDir = path.join(__dirname, `.${twerk_file_path}`);
     // const twerk_img = fs.readdirSync(imageDir);
 
