08eeec5d376ff110ed00c1aea12bc3898bfaee82
Adel Ansari <adelansari@github.com>
2021-11-26 17:37:13 +0200

    sussy recolor

--- src/commands/ImageProcessing/SusgifCommand.js
+++ src/commands/ImageProcessing/SusgifCommand.js
@@ -1,4 +1,4 @@
-const BaseCommand = require('../../utils/structures/BaseCommand');
+const BaseCommand = require("../../utils/structures/BaseCommand");
 const { Client, Message, MessageAttachment } = require("discord.js");
 const Canvas = require("canvas");
 const path = require("path");
@@ -7,26 +7,24 @@ const probe = require("probe-image-size");
 
 module.exports = class SusgifCommand extends BaseCommand {
   constructor() {
-    super('susgif', 'ImageProcessing', ["sus"]);
+    super("susgif", "ImageProcessing", ["sus"]);
   }
 
   async run(client, message, args) {
-    message.channel.send('susgif command works');
-
     if (message.author.bot) return false;
     if (message.attachments.size == 0) {
       return message.channel.send("No attachments in this message.");
     }
 
     // Saving the image sent by the user
-    const attachedImage = message.attachments.first().url; 
+    const attachedImage = message.attachments.first().url;
 
     // Finding dimensions:
     const dimensions = await probe(attachedImage);
     const sentImgWidth = dimensions.width;
     const sentImgHeight = dimensions.height;
 
-    // Rescaling the image based on width = 128 to create a canvas of "width x height: 128 x ?"
+    // Rescaling the image based on width = 32 to create a canvas of "width x height: 32 x N"
     const canvas_initial_width = 32;
     const canvas_initial_height = Math.round(
       (canvas_initial_width * sentImgHeight) / sentImgWidth
@@ -49,7 +47,50 @@ module.exports = class SusgifCommand extends BaseCommand {
     await message.channel.send("Rescaled Image:");
     await message.channel.send({ files: [rescaledImage] });
 
-    console.log(imgData)
 
+    // Creating a canvas for twerk images
+    const twerk_file_path = "/twerk_imgs/";
+    const susImageDir = path.join(__dirname, `.${twerk_file_path}0.png`);
+    const twerkSusImg = fs.readFileSync(`${susImageDir}`);
+    const twrkImgData = probe.sync(twerkSusImg);
+    // gif dimention:
+    const twerk_width = twrkImgData.width;
+    const twerk_height = twrkImgData.height;
+ 
+
+    const canvasTwerk = Canvas.createCanvas(
+      twerk_width,
+      twerk_height
+    );
+    const ctxTwerk = canvasTwerk.getContext("2d");
+    const backgroundTwerk = await Canvas.loadImage(`${susImageDir}`);
+    ctxTwerk.drawImage(backgroundTwerk, 0, 0, canvasTwerk.width, canvasTwerk.height);
+    const imgDataTwerk = ctxTwerk.getImageData(0, 0, canvasTwerk.width, canvasTwerk.height); // pixel data
+
+    const twImData = imgDataTwerk;  // twerk image data
+    for (var i = 0; i < twImData.data.length; i += 4) {
+      if (twImData.data[i]==214 && twImData.data[i+1]==224 && twImData.data[i+2]==240) {
+        twImData.data[i] = 255 ; // RED
+        twImData.data[i + 1] = 154 ; // GREEN
+        twImData.data[i + 2] = 118 ; // BLUE
+        twImData.data[i + 3] = 255; // ALPHA
+      }
+    }
+    
+    const twerkImage = new MessageAttachment(
+      canvasTwerk.toBuffer(),
+      "twerk.png"
+    );
+    await message.channel.send("Twerk Image:");
+    await message.channel.send({ files: [twerkImage] });
+
+    const recoloredSUS = canvasTwerk.getContext("2d");
+    recoloredSUS.putImageData(twImData, 0, 0);
+    const recolredSusImage = new MessageAttachment(
+      canvasTwerk.toBuffer(),
+      "recoloredSUS.png"
+    );
+    await message.channel.send("Recolored SUS Image:");
+    await message.channel.send({ files: [recolredSusImage] });
   }
-}
+};
